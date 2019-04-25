40228bc96c0fa8b0cced06c25286cf764987ebd5
Ivan Aranda <IvanAranda@iMac.local>
2019-04-25 17:30:43 -0400

    this are the last changes from imac

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 /*
  * Page 406 javanotes7 Textbook
  */
+import com.inc108.classes.IvanError;
 import com.inc108.classes.ParseError;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,12 +16,25 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    static String name = "Ivan";
     public static void main(String[] args){
-        try {
-            // TODO code application logic here
-            FileWriter writer = new FileWriter("/data.txt");
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        try {
+//            // TODO code application logic here
+//            FileWriter writer = new FileWriter("/data.txt");
+//        } catch (IOException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
+        try{
+            
+            if(name != "Ivan"){
+                System.out.println("name is wrong, throwing IvanError");
+                throw new IvanError("The name is wrong");
+            }else{
+                System.out.println("name is right");
+            }
+        }catch(IvanError ie){
+            System.out.println("caught the IvanError");
         }
     }
     
